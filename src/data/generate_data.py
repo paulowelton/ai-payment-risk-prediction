44eb6ddc553b4ea99fdec2da57a55aa2ae013d53
Paulo Welton <paulinho.welton08@gmail.com>
2026-04-06 11:09:50 -0300

    feat: uploading function to clean and process raw data into processed data

--- src/data/generate_data.py
+++ src/data/generate_data.py
@@ -3,73 +3,77 @@ import numpy as np
 from datetime import datetime, timedelta
 import random
 
-np.random.seed(42)
+def generate_data():
+    np.random.seed(42)
 
-NUM_CLIENTES = 200
-NUM_REGISTROS = 3000
+    NUM_CLIENTES = 200
+    NUM_REGISTROS = 3000
 
-clientes = [f"C{i}" for i in range(NUM_CLIENTES)]
+    clientes = [f'C{i}' for i in range(NUM_CLIENTES)]
 
-dados = []
+    dados = []
 
-for _ in range(NUM_REGISTROS):
-    cliente = random.choice(clientes)
-    
-    valor = np.random.randint(100, 5000)
-    
-    atraso = np.random.choice(
-        [0,1,2,5,10,15,30],
-        p=[0.5,0.1,0.1,0.1,0.1,0.05,0.05]
-    )
-    
-    data_pagamento = datetime.now() - timedelta(days=np.random.randint(0,180))
-    
-    forma_pagamento = random.choice(["boleto", "pix", "cartao"])
-    
-    qtd_pagamentos_anteriores = np.random.randint(0, 50)
-    
-    media_atraso_cliente = np.random.uniform(0, 15)
-    
-    cliente_novo = 1 if qtd_pagamentos_anteriores < 5 else 0
-    
-    pagou_em_dia = 1 if atraso == 0 else 0
-    
-    regiao = random.choice(["norte", "nordeste", "sul", "sudeste", "centro oeste"])
-    
-    score_credito = np.random.randint(0, 1000)
-    
-    tipo_empresa = random.choice(["MEI", "EI", "SLU", "LTDA", "S/A", "S/S"])
-    
-    dados.append([
-        cliente,
-        data_pagamento,
-        valor,
-        atraso,
-        forma_pagamento,
-        qtd_pagamentos_anteriores,
-        media_atraso_cliente,
-        cliente_novo,
-        pagou_em_dia,
-        regiao,
-        score_credito,
-        tipo_empresa
+    for _ in range(NUM_REGISTROS):
+        cliente = random.choice(clientes)
+        
+        valor = np.random.randint(100, 5000)
+        
+        atraso = np.random.choice(
+            [0,1,2,5,10,15,30],
+            p=[0.5,0.1,0.1,0.1,0.1,0.05,0.05]
+        )
+        
+        data_pagamento = datetime.now() - timedelta(days=np.random.randint(0,180))
+        
+        forma_pagamento = random.choice(['boleto', 'pix', 'cartao'])
+        
+        qtd_pagamentos_anteriores = np.random.randint(0, 50)
+        
+        media_atraso_cliente = np.random.uniform(0, 15)
+        
+        cliente_novo = 1 if qtd_pagamentos_anteriores < 5 else 0
+        
+        pagou_em_dia = 1 if atraso == 0 else 0
+        
+        regiao = random.choice(['norte', 'nordeste', 'sul', 'sudeste', 'centro oeste'])
+        
+        score_credito = np.random.randint(0, 1000)
+        
+        tipo_empresa = random.choice(['MEI', 'EI', 'SLU', 'LTDA', 'S/A', 'S/S'])
+        
+        dados.append([
+            cliente,
+            data_pagamento,
+            valor,
+            atraso,
+            forma_pagamento,
+            qtd_pagamentos_anteriores,
+            media_atraso_cliente,
+            cliente_novo,
+            pagou_em_dia,
+            regiao,
+            score_credito,
+            tipo_empresa
+        ])
+        
+    df = pd.DataFrame(dados, columns=[
+        'cliente_id',
+        'data_pagamento',
+        'valor',
+        'dias_atraso',
+        'forma_pagamento',
+        'qtd_pagamentos_anteriores',
+        'media_atraso_cliente',
+        'cliente_novo',
+        'pagou_em_dia',
+        'regiao',
+        'score_credito',
+        'tipo_empresa'
     ])
+
+    df.to_csv('data/raw/pagamentos.csv', index=False)
+
+    print('Dataset gerado com sucesso')
     
-df = pd.DataFrame(dados, columns=[
-    "cliente_id",
-    "data_pagamento",
-    "valor",
-    "dias_atraso",
-    "forma_pagamento",
-    "qtd_pagamentos_anteriores",
-    "media_atraso_cliente",
-    "cliente_novo",
-    "pagou_em_dia",
-    "regiao",
-    "score_credito",
-    "tipo_empresa"
-])
-
-df.to_csv("data/raw/pagamentos.csv", index=False, sep=';')
-
-print('Dataset gerado com sucesso')
+if __name__ == '__main__':
+    generate_data()
---
+++ src/processing/clean_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def load_data(path: str) -> pd.DataFrame:
+    return pd.read_csv(path)
+
+def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    df.columns = df.columns.str.lower().str.strip()
+    return df
+
+def convert_types(df: pd.DataFrame) -> pd.DataFrame:
+    df['data_pagamento'] = pd.to_datetime(df['data_pagamento'])
+    df['valor'] = df['valor'].astype(float)
+    return df
+
+def handle_missing(df: pd.DataFrame) -> pd.DataFrame:
+    return df.dropna()
+    
+def enconde_categorical(df: pd.DataFrame) -> pd.DataFrame:
+    df = pd.get_dummies(df, columns=['forma_pagamento', 'regiao', 'tipo_empresa'])
+    return df
+
+def process_data(input_path: str, output_path: str):
+    df = load_data(input_path)
+    print(df)
+    df = clean_column_names(df)
+    df = convert_types(df)
+    df = handle_missing(df)
+    df = enconde_categorical(df)
+    
+    df.to_csv(output_path, index=False)
+    
+    print('Dados processados com sucesso!')
+    
+if __name__ == '__main__':
+    process_data(
+        'data/raw/pagamentos.csv',
+        'data/processed/pagamentos_processados.csv'
+    )
